ec718aab9d85711d23844296e9670a3da3641aca
Deepakdass1326 <deepakkumardeepakkuma234@gmail.com>
2026-04-15 03:28:21 +0530

    feat: implement user authentication system with JWT and Google OAuth2 integration

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -3,6 +3,13 @@ import cookieParser from "cookie-parser"
 import morgan from "morgan"
 import authRouter from "./routes/auth.routes.js"
 import cors from "cors"
+import passport from "passport"
+import { Strategy as GoogleStrategy } from "passport-google-oauth20"
+import { config } from "./config/config.js"
+
+
+
+
 
 
 const app = express()
@@ -10,13 +17,22 @@ app.use(cors({
     origin: "http://localhost:5173",
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE"]
-}) )
+}))
+
+passport.use(new GoogleStrategy({
+    clientID: config.GOOGLE_CLIENT_ID,
+    clientSecret: config.GOOGLE_CLIENT_SECRET,
+    callbackURL: "http://localhost:5000/api/auth/google/callback"
+}, (accessToken, refreshToken, profile, done) => {
+    return done(null, profile)
+}))
 
 app.use(express.json())
 app.use(cookieParser())
 app.use(morgan("dev"))
+app.use(passport.initialize())
 
-app.use(express.urlencoded({ extended: true}))
+app.use(express.urlencoded({ extended: true }))
 
 
 
--- Backend/src/routes/auth.routes.js
+++ Backend/src/routes/auth.routes.js
@@ -1,12 +1,21 @@
 import { Router } from "express";
 import { loginValidation, registerValidation } from "../validation/auth.validator.js";
-import { register, login } from "../controllers/auth.controller.js";
+import { register, login, googleCallback } from "../controllers/auth.controller.js";
+import passport from "passport";
 
 const router = Router()
 
 
 router.post("/register", registerValidation, register)
 router.post("/login", loginValidation, login)
+router.get("/google",
+    passport.authenticate("google", { scope: ["profile", "email"] }))
+
+router.get("/google/callback",
+    passport.authenticate("google", { session: false }),
+    googleCallback
+)
+
 
 
 export default router
